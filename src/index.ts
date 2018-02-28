2d1ff6cf20a828f90991a7696cd38ab4b7b11b6b
belfz <marcinbar1@gmail.com>
2018-02-28 13:51:32 +0100

    adds tslint.json; removes unused dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { app } from 'hyperapp';
-import state from './state';
 import actions from './actions';
+import state from './state';
 import App from './views/App';
 
 app(state, actions, App, document.getElementById('app'));
